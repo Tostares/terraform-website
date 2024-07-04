f9610af580582355962f403e68ad5e1fa2e2190c
Rini Kilend <t.kilend@web.de>
2024-07-04 14:51:03 +0200

    created the security group for the Wordpress Instance and added rules

---
+++ sg.tf
@@ -0,0 +1,36 @@
+# Create Security Group for the Wordpress Instance
+
+resource "aws_security_group" "wordpress_sg" {
+  name        = "wordpress_sg"
+  description = "Security group for WordPress instance"
+
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.cidr_blocks
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = var.cidr_blocks
+  }
+
+  egress {
+    description = "All traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = var.cidr_blocks
+  }
+
+  tags = {
+    Name = "wordpress_sg ${var.tagNameDate}"
+  }
+}
