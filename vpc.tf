84725aea77f069ffda7f41343959b39315dfc3c0
Rini Kilend <t.kilend@web.de>
2024-07-04 14:08:22 +0200

    added the public and private route table association

--- vpc.tf
+++ vpc.tf
@@ -70,3 +70,18 @@ resource "aws_route_table" "private" {
   }
 
 }
+
+
+# Associate Public Subnets with Public Route Table
+resource "aws_route_table_association" "public_assoc" {
+  count          = length(var.public_subnet_cidr_blocks)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+# Associate Private Subnets with Private Route Tables
+resource "aws_route_table_association" "private_assoc" {
+  count          = length(var.private_subnet_cidr_blocks)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+}
