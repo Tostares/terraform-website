41f4795b35e20be6823e1e3c58b99f5823156634
Rini Kilend <t.kilend@web.de>
2024-07-04 14:34:27 +0200

    adjusted the gitignore to add a new terraform file, added the ec2 wordpress instance creation

---
+++ ec2.tf
@@ -0,0 +1,13 @@
+locals {
+  name = "WordPress Instance ${var.tagNameDate}"
+}
+#Get latest ami ID of Amazon Linux - values = ["al2023-ami-2023*x86_64"]
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023*x86_64"]
+  }
+}
