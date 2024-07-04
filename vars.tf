2b15aea1041e366c2ea3a5d4ab3837c008b0815a
Rini Kilend <t.kilend@web.de>
2024-07-04 11:52:01 +0200

    added the availability zones as well

--- vars.tf
+++ vars.tf
@@ -8,7 +8,12 @@ variable "cidr_blocks" {
   default = ["0.0.0.0/0"]
 }
 
+variable "availability_zones" {
+  description = "List of availability zones"
+  default     = ["us-west-2a", "us-west-2b"] # Replace with your availability zones
+}
 
+#Subnet Variable
 variable "public_subnet_cidr_blocks" {
   description = "CIDR blocks for public subnets"
   default     = ["10.0.0.0/26", "10.0.0.64/26"] # Adjust as needed
