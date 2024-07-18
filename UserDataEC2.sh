598dcacc2d06db094af0c33dbcdfd4d46942866f
Rini Kilend <t.kilend@web.de>
2024-07-18 11:38:46 +0200

    Reworked the database, no longer MariaDB now MySQL for compatibility reasons with RDS MySQL, reworked the wordpress install with fitting variables, added a default region, tried to slim down the code.

--- UserDataEC2.sh
+++ UserDataEC2.sh
@@ -1,65 +1,31 @@
-#!/bin/bash
+#! /bin/bash
 
-# Install updates
+# # Install updates
 sudo yum update -y
 
-# Install Apache server
+# Configure AWS CLI with IAM role credentials
+aws configure set default.region us-west-2
+
+#sudo yum install -y stress-ng
+
+#Install httpd
 sudo yum install -y httpd
 
-# Install MariaDB, PHP, and necessary tools
-sudo wget https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
-sudo yum localinstall -y mysql57-community-release-el7-11.noarch.rpm
+#Install PHP
+sudo amazon-linux-extras install -y php8.0
 
-# Import GPG key
-sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2022# Update repository metadata
-sudo yum clean all
-sudo yum makecache
-
-# Install MySQL server
-sudo yum install -y mysql-community-server
-
-# Start and enable MySQL service
-sudo systemctl start mysqld.service
-sudo systemctl enable mysqld.service
-
-# Retrieve the temporary root password
-temp_password=$(sudo grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
-
-# Set the desired root password
-DBRootPassword='root@258!Password'
-
-# Change the root password using the temporary password
-mysql -u root -p"$temp_password" --connect-expired-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DBRootPassword';"
-
-# Install PHP
-sudo amazon-linux-extras install -y php7.4
-
-# Update all installed packages
-sudo yum update -y
-
-# Restart Apache
-sudo systemctl restart httpd
-
-# Set database variables
-DBName="wordpress"
-DBUser="wordpress"
-DBPassword="admin1234"
-DBRootPassword="rootpassword1234"
-DBHost="localhost"
+# Retrieve RDS variables from Terraform output
+sudo touch DB_VAR.txt
+sudo chmod 777 DB_VAR.txt
+DBName=${db_name} >> DB_VAR.txt
+DBUser=${db_username} >> DB_VAR.txt
+#DBRootPassword='rootpassword'
+DBPassword=${db_password} >> DB_VAR.txt
+db_endpoint=${db_endpoint} >> DB_VAR.txt
 
 # Start Apache server and enable it on system startup
 sudo systemctl start httpd
 sudo systemctl enable httpd
-
-# Start MariaDB service and enable it on system startup
-sudo systemctl start mariadb
-sudo systemctl enable mariadb
-
-# Wait for MariaDB to fully start
-sleep 10
-
-# Set MariaDB root password
-sudo mysqladmin -u root password "$DBRootPassword"
 
 # Download and install WordPress
 sudo wget http://wordpress.org/latest.tar.gz -P /var/www/html
@@ -74,7 +40,7 @@ sudo cp ./wp-config-sample.php ./wp-config.php
 sudo sed -i "s/'database_name_here'/'$DBName'/g" wp-config.php
 sudo sed -i "s/'username_here'/'$DBUser'/g" wp-config.php
 sudo sed -i "s/'password_here'/'$DBPassword'/g" wp-config.php
-sudo sed -i "s/'localhost'/'$DBHost'/g" wp-config.php
+sudo sed -i "s/'localhost'/'$db_endpoint'/g" wp-config.php
 
 # Grant permissions
 sudo usermod -a -G apache ec2-user
@@ -88,3 +54,6 @@ echo "CREATE DATABASE IF NOT EXISTS $DBName;" | mysql -u root --password=$DBRoot
 echo "CREATE USER IF NOT EXISTS '$DBUser'@'localhost' IDENTIFIED BY '$DBPassword';" | mysql -u root --password=$DBRootPassword
 echo "GRANT ALL ON $DBName.* TO '$DBUser'@'localhost';" | mysql -u root --password=$DBRootPassword
 echo "FLUSH PRIVILEGES;" | mysql -u root --password=$DBRootPassword
+
+# Change the Wordpress configuration to direct connection instead of FTP
+sudo echo "define( 'FS_METHOD', 'direct' );" >> /var/www/html/wp-config.php
